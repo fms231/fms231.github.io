7526ff97e8d35cccaa45e818a896e5cbc7eea7a7
fms231 <1712541185@qq.com>
2024-01-31 22:47:12 +0800

    Site updated: 2024-01-31 22:47:11

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "125",
+    "top": "10", 
+    "key": "d9b8e0c768c34a89a8b21a00d029344c"
+  }
+}
